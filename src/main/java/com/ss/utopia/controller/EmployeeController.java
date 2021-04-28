72469f0c0284848adc0a2607371f9d595812952c
JonathanFrey0000 <82041401+JonathanFrey0000@users.noreply.github.com>
2021-04-28 15:57:11 +0200

    added view and update to EmployeeService, missing seats part

--- src/main/java/com/ss/utopia/controller/EmployeeController.java
+++ src/main/java/com/ss/utopia/controller/EmployeeController.java
@@ -1,14 +1,62 @@
 package com.ss.utopia.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ss.utopia.entity.Flight;
+import com.ss.utopia.entity.User;
+import com.ss.utopia.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
 
 @RestController
-@RequestMapping("/traveler")
+@RequestMapping("/utopia/employee")
 public class EmployeeController extends BaseController{
+
+    @Autowired
+    EmployeeService employeeService;
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
     }
+
+    @GetMapping("/flights/{id}")
+    public Flight getFlight(@PathVariable String id){
+        return employeeService.getFlight(id);
+    }
+
+    @PutMapping("/updateFlightRoute/{id}")
+    public Flight updateFlightRoute(@PathVariable String id, @RequestParam Integer routeID){
+        return employeeService.updateFlightRoute(id, routeID);
+    }
+
+    @PutMapping("/updateFlightAirplane/{id}")
+    public Flight updateFlightAirplane(@PathVariable String id, @RequestParam Integer airplaneID){
+        return employeeService.updateFlightAirplane(id, airplaneID);
+    }
+
+    @PutMapping("/updateFlightDepartTime/{id}")
+    public Flight updateFlightDepartTime(@PathVariable String id, @RequestParam Timestamp newTime){
+        return employeeService.updateFlightDepartTime(id, newTime);
+    }
+
+    @PutMapping("/updateFlightSeats/{id}")
+    public Flight updateFlightSeats(@PathVariable String id, @RequestParam Integer reservedSeats){
+        return employeeService.updateFlightSeats(id, reservedSeats);
+    }
+
+    @PutMapping("/updateFlightSeatPrice/{id}")
+    public Flight updateFlightSeatPrice(@PathVariable String id, @RequestParam Float seatPrice){
+        return employeeService.updateFlightSeatPrice(id, seatPrice);
+    }
+
+    @PutMapping("/updateFlight/{id}")
+    public Flight updateFlight(@PathVariable String id, @RequestParam Integer routeID, @RequestParam Integer airplaneID,
+                               @RequestParam Timestamp deparTime, @RequestParam Integer reservedSeats,
+                               @RequestParam Float seatPrice)
+    {
+        return employeeService.updateFlight(id, routeID, airplaneID,deparTime, reservedSeats, seatPrice);
+    }
+
+
 }
--- src/main/java/com/ss/utopia/service/EmployeeService.java
+++ src/main/java/com/ss/utopia/service/EmployeeService.java
@@ -1,4 +1,149 @@
 package com.ss.utopia.service;
 
+import com.ss.utopia.dao.AirplaneDAO;
+import com.ss.utopia.dao.FlightDAO;
+import com.ss.utopia.dao.RouteDAO;
+import com.ss.utopia.entity.Flight;
+import com.ss.utopia.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+
+@Service
+@Transactional(rollbackFor = { Exception.class })
 public class EmployeeService {
+
+    @Autowired
+    FlightDAO flightDAO;
+
+    @Autowired
+    RouteDAO routeDAO;
+
+    @Autowired
+    AirplaneDAO airplaneDAO;
+
+    public Flight getFlight(String id){
+        Integer flightID = Integer.parseInt(id);
+        Flight flight = null;
+        try {
+            flight = flightDAO.getFlightFromId(flightID);
+            flight.setRoute(routeDAO.getRouteById(flight.getRoute().getId()));
+            flight.setAirplane(airplaneDAO.getAirplaneById(flight.getAirplane().getId()));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return flight;
+
+    }
+    public Flight updateFlightRoute(String id, Integer newRouteID) {
+        Integer flightID = Integer.parseInt(id);
+        Flight flight = null;
+        try {
+            flight = flightDAO.getFlightFromId(flightID);
+            flight.setRoute(routeDAO.getRouteById(newRouteID));
+            flight.setAirplane(airplaneDAO.getAirplaneById(flight.getAirplane().getId()));
+            flightDAO.updateFlight(flight);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return flight;
+    }
+    public Flight updateFlightAirplane(String id, Integer newAirplaneID){
+        Integer flightID = Integer.parseInt(id);
+        Flight flight = null;
+        try {
+            flight = flightDAO.getFlightFromId(flightID);
+            flight.setRoute(routeDAO.getRouteById(flight.getRoute().getId()));
+            flight.setAirplane(airplaneDAO.getAirplaneById(newAirplaneID));
+            flightDAO.updateFlight(flight);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return flight;
+
+    }
+    public Flight updateFlightDepartTime(String id, Timestamp newDepartTime){
+        Integer flightID = Integer.parseInt(id);
+        Flight flight = null;
+        try {
+            flight = flightDAO.getFlightFromId(flightID);
+            flight.setRoute(routeDAO.getRouteById(flight.getRoute().getId()));
+            flight.setAirplane(airplaneDAO.getAirplaneById(flight.getAirplane().getId()));
+            flight.setDepartTime(newDepartTime);
+            flightDAO.updateFlight(flight);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return flight;
+
+    }
+    public Flight updateFlightSeats(String id, Integer newReservedSeats){
+        Integer flightID = Integer.parseInt(id);
+        Flight flight = null;
+        try {
+            flight = flightDAO.getFlightFromId(flightID);
+            flight.setRoute(routeDAO.getRouteById(flight.getRoute().getId()));
+            flight.setAirplane(airplaneDAO.getAirplaneById(flight.getAirplane().getId()));
+            flight.setReservedSeats(newReservedSeats);
+            flightDAO.updateFlight(flight);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return flight;
+
+    }
+    public Flight updateFlightSeatPrice(String id, Float newSeatPrice){
+        Integer flightID = Integer.parseInt(id);
+        Flight flight = null;
+        try {
+            flight = flightDAO.getFlightFromId(flightID);
+            flight.setRoute(routeDAO.getRouteById(flight.getRoute().getId()));
+            flight.setAirplane(airplaneDAO.getAirplaneById(flight.getAirplane().getId()));
+            flight.setSeatPrice(newSeatPrice);
+            flightDAO.updateFlight(flight);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return flight;
+
+    }
+
+    public Flight updateFlight(String id, Integer routeID, Integer airplaneID, Timestamp departTime, Integer reservedSeats, Float seatPrice)
+    {
+        Integer flightID = Integer.parseInt(id);
+        Flight flight = null;
+        try {
+            flight = flightDAO.getFlightFromId(flightID);
+
+            if (routeID != null)
+                flight.setRoute(routeDAO.getRouteById(routeID));
+            else
+                flight.setRoute(routeDAO.getRouteById(flight.getRoute().getId()));
+
+            if(airplaneID != null)
+                flight.setAirplane(airplaneDAO.getAirplaneById(airplaneID));
+            else
+                flight.setAirplane(airplaneDAO.getAirplaneById(flight.getAirplane().getId()));
+
+            if(departTime != null)
+                flight.setDepartTime(departTime);
+            if(reservedSeats != null)
+                flight.setReservedSeats(reservedSeats);
+            if(seatPrice != null)
+                flight.setSeatPrice(seatPrice);
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return flight;
+    }
 }
